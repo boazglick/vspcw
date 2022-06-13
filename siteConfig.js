d9764c2ae7e195c9b11e9b33c1bc197cd0d6e8b4
boazglick <95533522+boazglick@users.noreply.github.com>
2022-06-13 12:48:17 +0300

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -92,10 +92,10 @@ module.exports = {
   siteDescription:
     "indexparty",
   language: "he",
-  logoUrl: "logo.png",
-  iconUrl: "favicon.png",
+  logoUrl: "",
+  iconUrl: "",
   coverUrl: "cover.jpg",
-  alternateLogoUrl: "logo.png",
+  alternateLogoUrl: "",
   themeConfig: {
     variables: [
       {
