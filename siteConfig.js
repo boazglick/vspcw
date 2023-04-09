0b964c424a80444c483ad905a6322f8642760073
boazglick <95533522+boazglick@users.noreply.github.com>
2023-04-09 11:18:33 +0300

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "https://vspcw.com/vspcw", // Site domain. Do not include a trailing slash!
+  siteUrl: "https://vspcw.com", // Site domain. Do not include a trailing slash!
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
